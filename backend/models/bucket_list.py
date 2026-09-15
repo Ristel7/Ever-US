8d1fc620f221fafb358211998a02403bc31e7ef8
Priyanshu Kumar <Priyanshurajput7654@gmail.com>
2026-09-15 22:14:14 +0530

    added bucket list

---
+++ backend/models/bucket_list.py
@@ -0,0 +1,15 @@
+from database import db
+
+bucket_list_collection = db["bucket_list"]
+
+bucket_list_collection.create_index(
+    [("space_id", 1), ("created_at", -1)]
+)
+
+bucket_list_collection.create_index(
+    [("space_id", 1), ("completed", 1)]
+)
+
+bucket_list_collection.create_index(
+    [("author_id", 1)]
+)
